b9a30f267217ea8e29012fe55e06cc0737ba1dfd
TimeWz667 <timewz667@gmail.com>
2020-03-08 12:00:14 +0000

    Add definition data

---
+++ data-raw/DATASET.R
@@ -0,0 +1,8 @@
+## code to prepare `DATASET` dataset goes here
+
+usethis::use_data("DATASET")
+
+
+usethis::use_data(gp_type, gp_c_type, gp_t_type,
+                  gp_source, gp_c_source, gp_t_source,
+                  gp_phase, gp_c_phase, gp_t_phase)

4d34de2b2957e557a9234da5afcc09943d1cff3e
TimeWz667 <timewz667@gmail.com>
2020-03-08 12:01:08 +0000

    Modify raw data

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -6,3 +6,6 @@ usethis::use_data("DATASET")
 usethis::use_data(gp_type, gp_c_type, gp_t_type,
                   gp_source, gp_c_source, gp_t_source,
                   gp_phase, gp_c_phase, gp_t_phase)
+
+Survey <- pseudo
+usethis::use_data(Survey)
